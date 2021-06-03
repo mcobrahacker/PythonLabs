125f3f702bfc2b9d83f37e22446a105b841e97ca
Ivan Bondaruk <40137529+mcobrahacker@users.noreply.github.com>
2021-06-03 21:15:06 +0300

    1

--- Lab_10/lab_10_1.py
+++ Lab_10/lab_10_1.py
@@ -3,5 +3,101 @@
 # Розробити програму в якій за пунктом призначення визначається рейс і наявність білетів.
 
 class Ticket:
-    number = 0
+
+    def __init__(self):
+        self.arrive = {
+        "105" : "Madrid",
+        "211" : "London",
+        "721" : "Paris",
+        "429" : "New York",
+        "377" : "Rome"
+        }
+        self.time = {
+        "105" : "12:00",
+        "211" : "18:30",
+        "721" : "02:40",
+        "429" : "10:00",
+        "377" : "15:00"
+        }
+        self.free = {
+        "105" : 12,
+        "211" : 72,
+        "721" : 54,
+        "429" : 34,
+        "377" : 57
+        }
+        self.tickets = {
+        "105" : 5,
+        "211" : 23,
+        "721" : 15,
+        "429" : 2,
+        "377" : 14
+        }
+
+    def Counts(self):
+        print("Доступные рейсы:")
+        for k, v in self.arrive.items():
+            print(k)
+
+
+    def About_By_Arrive(self):
+        name = input("Введите пункт назначения: ")
+
+        key = ""
+        for k, v in self.arrive.items():
+            if v == name:
+                key = k
+
+        print(key)
+
+        if key in self.arrive:
+             if key in self.time:
+                 if key in self.free:
+                     if key in self.tickets:
+                         print("Пункт назначения - ", name , ", номер рейса - ", key , ", кол. билетов в наличии - ", self.tickets[key] )
+                         
+
+    def About(self):
+        name = input("Введите номер рейса: ")
+        if name in self.arrive:
+             if name in self.time:
+                 if name in self.free:
+                     if name in self.tickets:
+                         print("Номер рейса - ", name , ", пункт назначения - ", self.arrive[name] , ", время прибытия - ", self.time[name] , ", кол. свободных мест - ", self.free[name] , ", кол. билетов в наличии -", self.tickets[name] )
+
     
+
+ticket = Ticket()
+
+def Menu():
+    
+    print("Выберите пункт >>> ")
+    print("1 - Доступные рейсы")
+    print("2 - Информация о рейсе")
+    print("3 - Информация о рейсе по пункту назначения")
+    print("0 - Выход из программы.")
+
+def ChooseOption(par):
+
+
+    if par == 0:
+        var = 0
+    elif par == 1:
+        ticket.Counts()
+    elif par == 2:
+        ticket.About()
+    elif par == 3:
+        ticket.About_By_Arrive()
+    else:
+        print("Неправильная опция. Попробуйте другую! \n")
+
+var = 1
+
+while var == 1:
+
+    Menu()
+    par = int(input("Ваш выбор: "))
+    if par != 0:
+        ChooseOption(par)
+    else:
+        var = 0
--- Lab_8/2 module/lab_8_1.py
+++ Lab_8/2 module/lab_8_1.py
@@ -2,8 +2,69 @@
 # товари, їх кількість та ціну.
 
 
-opt1_2 = {"Cartofan" : (112,100), 
-          "Chlen" : (1111, 5), 
-          "Vasileostrovskie rebyata zalezayut na vetki, kidayut v detishek nozhi i monetki: " : (99999,99999), 
-         }
+dict_count = {
+    "Молоко" : 100,
+    "Сир" : 100,
+    "Творіг" : 100,
+    "Ковбаска" : 100,
+    "Перчик" : 100
+}
 
+dict_price = {
+    "Молоко" : 10,
+    "Сир" : 10,
+    "Творіг" : 10,
+    "Ковбаска" : 10,
+    "Перчик" : 10
+}
+
+def Find_Item_Name():
+    name = input("Введіть ім'я - ")
+    if name in dict_count:
+        if name in dict_price:
+            print(name, ", ціна - ", dict_price[name], ", кол. - ", dict_count[name])
+
+def Find_Item_Price():
+    name = input("Введіть ім'я - ")
+    if name in dict_count:
+        if name in dict_price:
+            print(name, ", ціна - ", dict_price[name])
+
+def Find_Item_Count():
+    name = input("Введіть ім'я - ")
+    if name in dict_count:
+        if name in dict_price:
+            print(name, ", кол. - ", dict_count[name])
+
+def Menu():
+    
+    print("Виберіть опцію")
+    print("1 - Знайти товар за назвою")
+    print("2 - Дізнатися ціну товару")
+    print("3 - Дізнатися кількість товару")
+    print("0 - вихід")
+
+def ChooseOption(par):
+
+
+    if par == 0:
+        var = 0
+    elif par == 1:
+        Find_Item_Name()
+    elif par == 2:
+        Find_Item_Price()
+    elif par == 3:
+        Find_Item_Count()
+    else:
+        print("Неправильна опція! Спробуйте іншу! \n")
+
+var = 1
+
+while var == 1:
+
+    Menu()
+    par = int(input("Ваш вибір: "))
+    if par != 0:
+        ChooseOption(par)
+    else:
+        var = 0
